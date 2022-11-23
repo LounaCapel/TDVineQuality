e6b3d561c7e7fed78e7d863235c912315c0d3967
LounaCapel <capellouna@eisti.eu>
2022-11-23 15:27:17 +0100

    signature

--- modelIA.py
+++ modelIA.py
@@ -6,20 +6,20 @@ from keras import models
 class Vin (BaseModel):
     fixedActivity : float
 
-def PredictionNote (vin : Vin):
+def PredictionNote(vin : Vin) -> int:
     return (0)
 
-def VinParfait ():
+def VinParfait() -> Vin:
     return ("")
 
-def ModeleSerialise (modele : models):
+def ModeleSerialise(modele : models) -> models :
     return()
 
-def Description ():
+def Description() -> str :
     return ("information")
 
-def Enrichir (modele : models,new : Vin ):
+def Enrichir(modele : models,new : Vin ) -> models :
     return (modele)
 
-def Entrainer (modele : models):
+def Entrainer(modele : models) -> models :
     return (modele)
